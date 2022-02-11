13f4a21b821284f2c68ef6bc101576d01283a936
Jiani Cao <jianicakes@gmail.com>
2022-02-10 16:52:58 -0800

    implement search function with property listings SQL query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -96,11 +96,58 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
+  const { city, owner_id, minPerNight: minimum_price_per_night, maxPerNight: maximum_price_per_night, rating: minimum_rating } = options;
+
+  let queryString = `
+    SELECT properties.*, AVG(rating) AS average_rating
+    FROM properties
+    JOIN property_reviews ON property_id = properties.id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += options.city ? `AND ` : `WHERE `;
+    queryString += `owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += options.city || options.owner_id ? `AND ` : `WHERE `;
+    queryString += `cost_per_night >= $${queryParams.length} ` ;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += options.city || options.owner_id || options.minimum_price_per_night ? `AND ` : `WHERE `;
+    queryString += `cost_per_night <= $${queryParams.length} ` ;
+  }
+
+  queryString += ` GROUP BY properties.id `;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING AVG(rating) >= $${queryParams.length}` ;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+  `;
+  console.log(queryString, queryParams);
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result) => result.rows)
     .catch((err) => {
       console.log(err.message);
+      return null;
     });
 };
 exports.getAllProperties = getAllProperties;
